9417ac8308f2f05978d08079f1c55fc4100a4815
shashank-jag <shashankupadhyaymailbox@gmail.com>
2018-03-29 10:39:00 +0530

    adding some datastructures

--- includes/node.h
+++ includes/node.h
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+#define successor finger[0];
+
 string my_ip, friend_ip;
 int my_port, friend_port;
+
+struct nodes{
+	string ip;
+	int port;
+	int nodeID;
+};
+
+vector<nodes> finger;
